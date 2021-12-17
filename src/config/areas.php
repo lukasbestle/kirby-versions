ff6f02667d9f9e032dd26bd070fc2b4370032acf
Lukas Bestle <mail@lukasbestle.com>
2021-12-17 22:11:59 +0100

    Fix "user-not-logged-in" error on Panel login
    Closes #7.

--- src/config/areas.php
+++ src/config/areas.php
@@ -1,10 +1,16 @@
 <?php
 
+use Kirby\Exception\LogicException;
 use LukasBestle\Versions\Plugin;
 
 return [
     'versions' => function ($kirby) {
-        $accessPermission = Plugin::instance()->hasPermission('access');
+        try {
+            $accessPermission = Plugin::instance()->hasPermission('access');
+        } catch (LogicException $e) {
+            // area was loaded by Kirby without a logged-in user
+            $accessPermission = false;
+        }
 
         return [
             'label' => t('view.versions'),
