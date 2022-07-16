e540c2a001a643d6906b3efd9c87305f406bfeb2
Lukas Bestle <mail@lukasbestle.com>
2022-07-16 13:00:47 +0200

    Switch to Prettier for frontend coding style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,24 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/recommended"
-    ],
-    "globals": {
-        "panel": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "vue/require-default-prop": "off",
-        "vue/attributes-order": "error",
-        "vue/require-prop-types": "error",
-        "vue/max-attributes-per-line": [
-            "error",
-            {
-                "singleline": 3,
-                "multiline": {
-                    "max": 1,
-                    "allowFirstLine": false
-                }
-            }
-        ],
-        "vue/html-closing-bracket-newline": [
-            "error",
-            {
-                "singleline": "never",
-                "multiline": "always"
-            }
-        ]
-    }
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/recommended",
+		"prettier"
+	],
+	globals: {
+		"panel": true
+	},
+	rules: {
+		"vue/attributes-order": "error",
+		"vue/component-definition-name-casing": "off",
+		"vue/html-closing-bracket-newline": [
+			"error",
+			{
+				singleline: "never",
+				multiline: "always"
+			}
+		],
+		"vue/multi-word-component-names": "off",
+		"vue/require-default-prop": "off",
+		"vue/require-prop-types": "error"
+	}
 };
